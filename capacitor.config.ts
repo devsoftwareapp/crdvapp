2dd8eaccd3df88c3872ef7b87e932ac7a2b6e75c
devsoftwareapp <devsoftwaremail@gmail.com>
2025-11-27 22:43:47 +0300

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/core';
+
+const config: CapacitorConfig = {
+  appId: 'com.devsoftware.pdfreader',
+  appName: 'PDF Reader',
+  webDir: 'web',
+  bundledWebRuntime: false
+};
+
+export default config;
